925a5dd60cde17bfd40af9095e70fbaba404be4d
Santiago Mansilla <smansilla@decampoacampo.com>
2026-01-24 22:40:38 -0300

    fixes proxies + services

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -126,8 +126,8 @@
     </main>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('js/drag-drop.js') }}"></script>
-    <script src="{{ asset('js/notifications.js') }}"></script>
+    <script src="{{ secure_asset('js/drag-drop.js') }}"></script>
+    <script src="{{ secure_asset('js/notifications.js') }}"></script>
     @stack('scripts')
 </body>
 </html>
